26bc215fcc47a5e04f8579d20334b339ffca231a
Md Waseem Akram <mdwaseem0603@gmail.com>
2023-06-18 19:40:58 +0530

    done

--- FitQuest/frontend/src/pages/Workout/Singleworoutpage/Singleworkout.jsx
+++ FitQuest/frontend/src/pages/Workout/Singleworoutpage/Singleworkout.jsx
@@ -11,36 +11,59 @@ import { BsSearch } from "react-icons/bs";
 const Singleworkout = () => {
   const [data, setdata] = useState();
   const { id } = useParams();
-  const [search,setSearch]=useState("")
-
+  const [search, setSearch] = useState("");
+  const [ser, setser] = useState("");
+  console.log(ser);
   useEffect(() => {
-    axios
-      .get(baseURL+`/workout/${id}`)
-      .then((response) => {
-        setdata(response.data[id]);
-      });
+    axios.get(baseURL + `/workout/${id}`).then((response) => {
+      setdata(response.data[id]);
+    });
   }, []);
   console.log(data);
-  const handleSearch=()=>{
-     fetch(`https://fitquestbackend.onrender.com/workout/all?search=${search}`).then((res)=>res.json()).then((res)=>{
-      setdata(res.all)
-     }).catch((err)=>{
-      console.log(err)
-     })
-  }
+  const handleSearch = () => {
+    fetch(`https://fitquestbackend.onrender.com/workout/all?search=${search}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setdata(res.all);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <br />
-      <Flex width={"40%"} margin={"auto"} pr={"10px"} alignItems={"center"} gap={"10px"} border={"1px solid black"} borderRadius={"20px"}>
-      <Input placeholder="Search workout here...!" border={"none"} borderRight={"1px solid black"} borderRadius={"none"} onChange={(e)=>setSearch(e.target.value)}/>
-      <BsSearch size={"25px"} onClick={handleSearch}/>
+      <Flex
+        width={"40%"}
+        margin={"auto"}
+        pr={"10px"}
+        alignItems={"center"}
+        gap={"10px"}
+        border={"1px solid black"}
+        borderRadius={"20px"}>
+        <Input
+          placeholder="Search workout here...!"
+          border={"none"}
+          borderRight={"1px solid black"}
+          borderRadius={"none"}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            setser(e.target.value);
+          }}
+        />
+        <BsSearch size={"25px"} onClick={handleSearch} />
       </Flex>
       <br />
-      {data?.length>0?
-      data.map((it,ind) => (
-          <Details key={ind}  {...it} />
-      )):<Box textAlign={"center"}> <Text as={"b"}>No result found for this {search} query</Text> </Box>
-      }
+      {data?.length > 0 ? (
+        data
+          .filter((it) => it.title.toLowerCase().includes(ser.toLowerCase()))
+          .map((it, ind) => <Details key={ind} {...it} />)
+      ) : (
+        <Box textAlign={"center"}>
+          {" "}
+          <Text as={"b"}>No result found for this {search} query</Text>{" "}
+        </Box>
+      )}
     </div>
   );
 };
