c8d63b1ac2668297a03cf23ca1c648576abc34e7
izhar100 <ezharashraf523@gmail.com>
2023-06-18 20:52:28 +0530

    some minor error solved

--- FitQuest/frontend/src/components/Navbar.jsx
+++ FitQuest/frontend/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
       <Box bgColor={"#fdfdfd"} boxShadow={"rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px"}>
         <Flex p={"10px"}>
           <Flex width={"40%"}>
-            <Image src={logo} width={"200px"} />
+            <Image src={logo} width={"200px"} onClick={()=>navigate("/")}/>
           </Flex>
           <Flex width={'60%'} justifyContent={"space-between"} alignItems={"center"} display={{ xl: "flex", lg: "flex", md: "flex", sm: "none", base: "none" }}>
             <Text as={"b"} onClick={() => navigate("/")} _hover={{cursor:"pointer"}}>Dashboard</Text>
